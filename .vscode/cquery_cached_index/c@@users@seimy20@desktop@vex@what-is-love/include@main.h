bdf688240559128b38fd84d275b3e20e68180442
syonatan <38823174+syonatan@users.noreply.github.com>
2019-02-22 17:54:05 -0800

    saving to sd card
    saving to sd card

---
+++ .vscode/cquery_cached_index/c@@users@seimy20@desktop@vex@what-is-love/include@main.h
@@ -0,0 +1,54 @@
+#ifndef _PROS_MAIN_H_
+#define _PROS_MAIN_H_
+
+#define PROS_USE_SIMPLE_NAMES
+
+#define PROS_USE_LITERALS
+
+#include "api.h"
+#include "drive.hpp"
+#include "intake.hpp"
+#include "launcher.hpp"
+//#include "adjuster.hpp"
+#include "lift.hpp"
+#include "pros/apix.h"
+#include "gui.hpp"
+
+//ports
+#define LEFTFRONT 17
+#define LEFTREAR 16
+#define RIGHTFRONT 15
+#define RIGHTREAR 14
+#define LAUNCHER 13
+#define ADJUSTER 18
+#define LIFT 19
+#define INTAKE 20
+
+
+using namespace pros;
+using namespace pros::literals;
+
+extern Controller master;
+extern bool mirror;
+extern int auton;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void autonomous(void);
+void initialize(void);
+void disabled(void);
+void competition_initialize(void);
+void opcontrol(void);
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+/**
+ * You can add C++-only headers here
+ */
+//#include <iostream>
+#endif
+
+#endif  // _PROS_MAIN_H_
---
+++ .vscode/cquery_cached_index/c@@users@seimy20@desktop@vex@what-is-love/src@intake.cpp
@@ -0,0 +1,91 @@
+#include "main.h"
+
+static int intakeTarget = 0;
+
+//motors
+Motor intake1(INTAKE, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_DEGREES);
+
+//line sensors
+ADILineSensor line_L('F');
+ADILineSensor line_R('E');
+ADILineSensor line_I('H');
+
+
+/**************************************************/
+//basic control
+void intake(int vel){
+  intake1.move_velocity(vel);
+}
+
+/**************************************************/
+//feedback
+bool hasBall(){
+  if(line_L.get_value() < 2000 || line_R.get_value() < 2000)
+    return true;
+  else
+    return false;
+}
+
+bool isLoaded(){
+  if(line_I.get_value() < 2000)
+    return true;
+  else
+    return false;
+}
+
+/**************************************************/
+//autonomous control
+void intakeBallAsync(){
+  intakeTarget = 1;
+}
+
+void loadBallAsync(){
+  intakeTarget = 2;
+}
+
+void intakeBall(){
+  intake(127);
+  while(!hasBall()) delay(20);
+  intake(0);
+}
+
+void loadBall(){
+  intake(127);
+  while(!isLoaded()) delay(20);
+  intake(0);
+}
+
+/**************************************************/
+//task control
+void intakeTask(void* parameter){
+  while(1){
+    delay(20);
+
+    switch(intakeTarget){
+      case 1:
+        intakeBall();
+        break;
+      case 2:
+        loadBall();
+        break;
+    }
+
+    intakeTarget = 0;
+  }
+}
+
+/**************************************************/
+//operator control
+void intakeOp(){
+  static int vel;
+
+
+  intake(vel);
+
+  if(master.get_digital(DIGITAL_R1))
+    vel = 127;
+  else if(master.get_digital(DIGITAL_L1))
+    vel = -60;
+  else
+    vel = 0;
+}
