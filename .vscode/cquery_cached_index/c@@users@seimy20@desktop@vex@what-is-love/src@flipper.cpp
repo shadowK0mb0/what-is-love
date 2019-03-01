5b482c8f75af3e401bf20a7dda05f307895345ac
syonatan <38823174+syonatan@users.noreply.github.com>
2019-02-28 21:20:07 -0800

    auton and flipper

--- .vscode/cquery_cached_index/c@@users@seimy20@desktop@vex@what-is-love/src@flipper.cpp
+++ .vscode/cquery_cached_index/c@@users@seimy20@desktop@vex@what-is-love/src@flipper.cpp
@@ -8,11 +8,12 @@ void flip(int vel){
 
 void flipperOp(){
   static int vel;
-  if(master.get_digital(DIGITAL_L1) && flipper.get_raw_position(NULL) > 0) // checks that the flipper is not dangerously far down
+  if(master.get_digital(DIGITAL_L1) && flipper.get_position() < 0){ // checks that the flipper is not dangerously far down
     vel = 110;
-  else if(master.get_digital(DIGITAL_L2) && flipper.get_raw_position(NULL) < 550) // checks that the flipper is not dangerously far up
+  }else if(master.get_digital(DIGITAL_L2) && flipper.get_position() > -550){ // checks that the flipper is not dangerously far up
     vel = -80;
-  else
+  }else{
     vel = 0;
+  }
   flip(vel);
 }
