93330b615b76c03cb3f7f02eab27fbaf0594a217
syonatan <38823174+syonatan@users.noreply.github.com>
2019-02-28 19:56:42 -0800

    small flipper fixes

--- .vscode/cquery_cached_index/c@@users@seimy20@desktop@vex@what-is-love/src@flipper.cpp
+++ .vscode/cquery_cached_index/c@@users@seimy20@desktop@vex@what-is-love/src@flipper.cpp
@@ -8,9 +8,9 @@ void flip(int vel){
 
 void flipperOp(){
   static int vel;
-  if(master.get_digital(DIGITAL_L1) && flipper.get_raw_position(NULL) < 550)
+  if(master.get_digital(DIGITAL_L1) && flipper.get_raw_position(NULL) > 0) // checks that the flipper is not dangerously far down
     vel = 110;
-  else if(master.get_digital(DIGITAL_L2) && flipper.get_raw_position(NULL) > 0)
+  else if(master.get_digital(DIGITAL_L2) && flipper.get_raw_position(NULL) < 550) // checks that the flipper is not dangerously far up
     vel = -80;
   else
     vel = 0;
